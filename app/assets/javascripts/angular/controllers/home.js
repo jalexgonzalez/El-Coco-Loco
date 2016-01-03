0f94ac164da68e70a25d3314ba8239010f4fd355
Nicholas Gonzalez <nicky93@gmail.com>
2016-01-03 13:04:36 -0500

    Adding more highlights

--- app/assets/javascripts/angular/controllers/home.js
+++ app/assets/javascripts/angular/controllers/home.js
@@ -29,10 +29,12 @@ angular.module('elCocoLocoApp')
     $scope.slides.push(slide1, slide2, slide3, slide4, slide5);
 
     $scope.highlights = [
+      {text: 'Breakfast, Lunch, & Dinner from 7am to 10pm', sref: 'menu'},
       {text: 'Low Carb Friendly Menu', sref: 'menu'},
       {text: 'Homemade Premium Ice Cream & Sorbet', sref: 'menu'},
       {text: 'Find The Nearest Location', sref: 'locations'},
-      {text: 'Breakfast, Lunch, & Dinner from 7am to 10pm', sref: 'locations'}
+      {text: 'Catering Available, Call Us!', sref: 'locations'},
+      {text: 'Ice Cream Parties, Call Us!', sref: 'locations'}
     ];
 
   }]);
